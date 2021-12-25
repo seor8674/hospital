fe8eb0280eac8421d010864808ef266d65a3d8aa
DESKTOP-6545LE9\82107 <hwanjuni70@gmail.com>
2021-12-25 21:16:27 +0900

    no message

---
+++ hospital/src/main/java/com/example/hospital/config/SwaggerConfig.java
@@ -0,0 +1,27 @@
+package com.example.hospital.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.RequestHandler;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import javax.tools.DocumentationTool;
+import java.lang.annotation.Documented;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+
+    @Bean
+    public Docket api(){
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.example.hospital.web.controller"))
+                .paths(PathSelectors.any())
+                .build();
+    }
+}
